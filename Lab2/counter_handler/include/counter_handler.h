3c969ac29b23bbaa28d47548af3957e2daea6c1b
AlfredoSoto1 <alfredo.soto2@upr.edu>
2024-09-11 22:51:39 -0400

    updated comments

--- Lab2/counter_handler/include/counter_handler.h
+++ Lab2/counter_handler/include/counter_handler.h
@@ -3,6 +3,9 @@
 
 typedef int MenuSelection;
 
+/**
+ * @brief Contains the input arguments as a list of processes
+ */
 typedef struct InputArguments {
   int count;
   int* program_ids;
@@ -20,19 +23,46 @@ enum Selection { STOP = 1, CONTINUE, CHANGE, KILL, EXIT };
  */
 MenuSelection DisplayMenu();
 
+/**
+ * @brief Handles the process selection
+ *
+ * @param arguments
+ */
 int HandleProcessSelection(InputArguments* arguments);
 
 /**
  * @brief Handles the stop selection
+ *
+ * @param arguments
  */
 void HandleStopSelection(InputArguments* arguments);
 
+/**
+ * @brief Handles the continue selection
+ *
+ * @param arguments
+ */
 void HandleContinueSelection(InputArguments* arguments);
 
+/**
+ * @brief Hanbdles the change direction selection
+ *
+ * @param arguments
+ */
 void HandleChangeSelection(InputArguments* arguments);
 
+/**
+ * @brief Handles the killing of a process section
+ *
+ * @param arguments
+ */
 void HandleKillSelection(InputArguments* arguments);
 
+/**
+ * @brief Checks if any of the given processes are alive
+ *
+ * @return 1 if atleast one process is alive
+ */
 int AtleastOneAlive(InputArguments* arguments);
 
 #endif  // COUNTER_HANDLER_H
