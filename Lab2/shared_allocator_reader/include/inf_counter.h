115305785004a3c01f13fcc50ecb01325c7096bc
AlfredoSoto1 <alfredo.soto2@upr.edu>
2024-09-13 17:07:22 -0400

    reader

---
+++ Lab2/shared_allocator_reader/include/inf_counter.h
@@ -0,0 +1,4 @@
+#ifndef INF_COUNTER_H
+#define INF_COUNTER_H
+
+#endif  // INF_COUNTER_H
---
+++ Lab2/shared_allocator_reader/src/main.c
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main() {
+  key_t key
+
+      return 0;
+}
