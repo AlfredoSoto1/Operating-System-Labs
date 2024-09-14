08146145112eb950954f30790578cc3f6762cacf
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2024-09-14 18:00:42 -0400

    Updated exercise 1

--- Lab2/children_spawner/src/spawner.c
+++ Lab2/children_spawner/src/spawner.c
@@ -14,11 +14,36 @@ int SpawnChildren(const char* program) {
   // Print the program id of the child
   printf("I'm a child, and my id is: %d\n", getpid());
 
-  // Transform process into Greetings program
-  execlp(program, "greetings_process", NULL);
+  // Create the first greetings grandson
+  int grand_son1_err;
+  int grand_son1 = fork();
 
-  // Exit with parent with failure if exec() fails
-  exit(EXIT_FAILURE);
+  if (grand_son1 == 0) {
+    // Transform process into Greetings program
+    execlp(program, "greetings_process", NULL);
+
+    // Exit with parent with failure if exec() fails
+    exit(EXIT_FAILURE);
+  }
+
+  // Create the second greetings grandson
+  int grand_son2_err;
+  int grand_son2 = fork();
+
+  if (grand_son2 == 0) {
+    // Transform process into Greetings program
+    execlp(program, "greetings_process", NULL);
+
+    // Exit with parent with failure if exec() fails
+    exit(EXIT_FAILURE);
+  }
+
+  // Wait for both grandsons
+  waitpid(grand_son1, &grand_son1_err, 0);
+  waitpid(grand_son2, &grand_son2_err, 0);
+
+  // The children ends successfully
+  exit(EXIT_SUCCESS);
 
   // This return should never happen
   return -1;
@@ -36,7 +61,7 @@ void WaitForChildren(int* processes, const unsigned int process_count) {
   }
 
   // Log information to the console
-  printf("All processses have finished\n");
+  // printf("All processses have finished\n");
 
   for (int i = 0; i < process_count; i++) {
     printf("Process child [%d] exited with: %d\n", processes[i],
