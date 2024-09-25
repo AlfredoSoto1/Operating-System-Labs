3c6b220176ba875fec17395d0bf186354da5735c
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2024-09-25 16:33:04 -0400

    yes

--- Lab3/two_threads/src/main.c
+++ Lab3/two_threads/src/main.c
@@ -6,38 +6,37 @@
 int exit_flag = 0;
 float number = -1;
 pthread_mutex_t mutex;
+pthread_cond_t allow_listener;
+pthread_cond_t allow_computation;
 
 void* ListenerThread(void* param) {
   do {
     pthread_mutex_lock(&mutex);
+    // Print prompt
     printf(
         "Enter a floating point number between [0, 1] to calculate it's "
         "arcsine:\n");
-
+    // Read number from console
     scanf("%f", &number);
+    // Wait for computation to be done
+    pthread_cond_wait(&allow_computation, &mutex);
 
-    if (number > 1.0f) {
-      exit_flag = 1;
-    }
     pthread_mutex_unlock(&mutex);
-
-  } while (!exit_flag);
+  } while (1);
 }
 
 void* ArcsineThread(void* param) {
   do {
+    pthread_mutex_lock(&mutex);
     if (number >= 0.0f && number <= 1.0f) {
-      pthread_mutex_lock(&mutex);
+      // Compute the arcsine of the entered float number
       float result = asin(number);
-      printf("Arcsine of %.4f is %.4f\n", number, result);
-
-      // Make the number negative to stop this thread from spamming into the
-      // console the value
-      number = -1;
-      pthread_mutex_unlock(&mutex);
+      printf("Computation of asin(%.4f) = %.4f\n", number, result);
     }
-
-  } while (!exit_flag);
+    // Let know the Listener that he can ask the user again
+    pthread_cond_signal(&allow_computation);
+    pthread_mutex_unlock(&mutex);
+  } while (1);
 }
 
 int main() {
