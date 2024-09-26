1bdd2bd86865679c8f600ae5a18060f72a38774d
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2024-09-26 15:05:55 -0400

    update

--- Lab3/square_root/src/main.c
+++ Lab3/square_root/src/main.c
@@ -13,6 +13,11 @@ void* BufferHandler(void* param) {
   // individually and storing them in an array named input_buffer using an index
   // named n_read_values to indicate how many values have been read from the
   // file.
+  long buffer_length = 0;
+  long n_read_values = 0;
+  unsigned char* input_buffer = malloc(sizeof(unsigned char) * buffer_length);
+
+  // Read data from file and store it on the buffer
 }
 
 void* CalculateSqrt(void* param) {
@@ -32,19 +37,44 @@ void* CalculateSqrt(void* param) {
   // index named n_processed_values.
 }
 
+double SqrtIter(double number, int n) {
+  double guess = number / 2.0;
+
+  for (int i = 0; i < n; i++) {
+    guess = 0.5 * (guess + number / guess);
+  }
+
+  return guess;
+  // if (n == 0) {
+  //   return guess;
+  // }
+
+  // double better_guess = SqrtIter(number, guess / 2.0, n - 1) +
+  //                       number / SqrtIter(number, guess / 2.0, n - 1);
+  // return 0.5 * (better_guess);
+}
+
 void* FileHandlerOutput(void* param) {
   // Finally, the third thread gets the square root data and stores them in the
   // file sqrt.txt.
 }
 
-int main() {
+void BootFileHandler() {
   pthread_t file_handler_thread;
 
   int file_status;
 
   pthread_create(&file_handler_thread, NULL, FileHandlerPrepare, &file_status);
-
   pthread_join(file_handler_thread, NULL);
 
+  if (file_status) {
+    printf("File exists. Proceeding to do computation...\n");
+  } else {
+    printf("File buffer handled correctly...\n");
+  }
+}
+
+int main() {
+  //
   return 0;
 }
