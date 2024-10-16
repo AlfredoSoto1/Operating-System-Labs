9f3112a5c015ad698a4b102135acc35428cdc0ed
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2024-10-16 18:16:21 -0400

    lab4

--- bono_examen_1/imu_driver/include/imu_driver.h
+++ bono_examen_1/imu_driver/include/imu_driver.h
@@ -20,17 +20,55 @@ typedef struct {
   double acc_y;
 } BotState;
 
+/**
+ * @brief Initializes the bot state with the default set values.
+ *
+ * @param state
+ */
 void InitBotState(BotState* state);
 
+/**
+ * @brief Updates the bot state with the movement formulas.
+ *
+ * @param state
+ */
 void UpdateBotState(BotState* state);
+
+/**
+ * @brief Polls the signal received every 200ms from the timer signal.
+ *
+ * @param imu_file
+ */
 void PollSignalEvent(FILE* imu_file);
 
+/**
+ * @brief Reads the last state written into the imu_data
+ *
+ * @param imu_file
+ * @param state
+ * @return a positive number if has read successfully
+ */
 int ReadPreviousState(FILE* imu_file, BotState* state);
 
+/**
+ * @brief Writes the current bot state into the imu_data,
+ *
+ * @param file
+ * @param state
+ */
 void WriteCurrentState(FILE* file, BotState* state);
 
+/**
+ * @brief Accesses the imu file if it exists. If it doesnt, it will create it.
+ *
+ * @return imu_data file
+ */
 FILE* AccessImuData();
 
+/**
+ * @brief Launches the imu_driver app
+ *
+ */
 void LaunchImuDriver();
 
 #endif  // IMU_DRIVER_H
