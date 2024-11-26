1a66e130797ca2b568aa109b6e823705504ce1ea
AlfredoSoto1 <alfredo.soto2@upr.edu>
2024-11-25 20:29:42 -0400

    Driver working on reading data from mouse

--- project/mouse_saver/src/MouseDriver.c
+++ project/mouse_saver/src/MouseDriver.c
@@ -1,20 +1,66 @@
 #include "MouseDriver.h"
 
+#include <errno.h>
 #include <fcntl.h>
 #include <linux/input.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/resource.h>  // for setpriority
+#include <sys/wait.h>
 #include <unistd.h>
 
-int DriverInit(MouseDriver* driver) {
+static MouseDriver driver;
+
+void InitMouseDriver() {
   // Generic mouse device file
-  driver->device = "/dev/input/mice";
+  driver.device = "/dev/input/mice";
 
   // Open the mice file content
-  int fd = open(driver->device, O_RDONLY);
-  if (fd == -1) {
+  driver.f_device = open(driver.device, O_RDONLY);
+  if (driver.f_device == -1) {
     perror("Error opening device");
-    return -1;
+    exit(EXIT_FAILURE);
   }
 
-  return fd;
+  // Prepare interruption signal to handle driver resources when program ends.
+  struct sigaction action;
+  action.sa_flags = 0;
+  action.sa_handler = HandleMouseCleanup;
+  sigaction(SIGINT, &action, NULL);
+}
+
+void DisposeMouseDriver() {
+  // Close the file reference device to mouse data
+  close(driver.f_device);
+}
+
+void HandleMouseCleanup(int signal) {
+  char* debug = "Program Terminated successfully.\n";
+  write(STDOUT_FILENO, debug, strlen(debug));
+
+  DisposeMouseDriver();
+
+  exit(EXIT_SUCCESS);
+}
+
+void GetMouse() {
+  // 3 bytes of data reserved for storing mouse delta position and mouse button
+  // actions.
+  signed char data[3];
+
+  if (read(driver.f_device, data, sizeof(data)) > 0) {
+    char left = data[0] & 0x1;    // mask for 1rst bit
+    char right = data[0] & 0x2;   // mask for 2snd bit
+    char middle = data[0] & 0x4;  // mask for 4rth bit
+
+    // Mouse delta position
+    int dx = data[1];
+    int dy = data[2];
+
+    printf("Left: %d, Right: %d, Middle: %d, dx: %d, dy: %d\n", left, right,
+           middle, dx, dy);
+  }
 }
--- project/mouse_saver/src/main.c
+++ project/mouse_saver/src/main.c
@@ -1,62 +1,12 @@
-#include <fcntl.h>
-#include <linux/input.h>
-#include <stdio.h>
-#include <unistd.h>
-
-void printb(int number) {
-  for (int i = 32 - 1; i >= 0; --i) {
-    printf("%d", (number >> i) & 1);
-  }
-  printf("\n");
-}
-
-void capture() {
-  const char* device = "/dev/input/mice";  // Generic mouse device file
-  int fd = open(device, O_RDONLY);
-  if (fd == -1) {
-    perror("Error opening device");
-    return -1;
-  }
-
-  signed char data[3];
-
-  printf("Listening for mouse events. Move the mouse or press buttons.\n");
-  while (1) {
-    if (read(fd, data, sizeof(data)) > 0) {
-      int left = data[0] & 0x1;
-      int right = data[0] & 0x2;
-      int middle = data[0] & 0x4;
-      int dx = data[1];
-      int dy = data[2];
-
-      printf("Left: %d, Right: %d, Middle: %d, dx: %d, dy: %d\n", left, right,
-             middle, dx, dy);
-    }
-  }
-
-  close(fd);
-}
-
+#include "MouseDriver.h"
 int main() {
-  unsigned char x1 = 2;
-  unsigned char y1 = 5;
+  // Create a new mouse driver attached to the IO device from linux
+  InitMouseDriver();
 
-  unsigned char x2 = 7;
-  unsigned char y2 = 9;
+  while (1) {
+    GetMouse();
+  }
 
-  int packed_x = (x1 << 8 | x2);
-  int packed_y = (y1 << 8 | y2);
-
-  long packed = (packed_x << 16 | packed_y);
-
-  // printb(packed_x);
-  // printb(packed_y);
-  // printb(packed_x + packed_y);
-
-  int result = packed_x + packed_y;
-  printf("%d\n", result & 0xFF);
-  printf("%d\n", (result >> 8) & 0xFF);
-  printf("%d\n", (result >> 16) & 0xFF);
-  printf("%d\n", (result >> 24) & 0xFF);
+  DisposeMouseDriver();
   return 0;
 }
